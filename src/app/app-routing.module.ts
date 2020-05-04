2598a7e39c29935c3e6646b6597a6ff24524aa3e
faiana <biafriedrich06@gmail.com>
2020-05-04 22:13:56 +0100

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {NavbarComponent} from './navbar/navbar.component';
+import {WelcomeComponent} from './welcome/welcome.component';
 
 
-const routes: Routes = [];
-
+/*Angular path routes*/
+const routes: Routes = [
+  { path: 'navbar', component: NavbarComponent },
+  { path: 'welcome', component: WelcomeComponent },
+  // { path: 'users', component: UserComponent },
+  { path: '', redirectTo: '/welcome', pathMatch: 'full' }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { UserHomepageComponent } from './user-homepage/user-homepage.component';
+import { DialogModule } from 'primeng/dialog';
+import { InputTextModule } from 'primeng/inputtext';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    WelcomeComponent,
+    UserHomepageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ToolbarModule,
+    ButtonModule,
+    DialogModule,
+    InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
