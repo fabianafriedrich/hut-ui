4779a43cb925972771007ebdbf9c1ec2ae508f08
faiana <biafriedrich06@gmail.com>
2020-05-19 07:57:46 +0100

    Changing config to deploy UI in heroku plataform.

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/dist/hut-ui'));
+app.get('/*', function (req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+
+  res.sendFile(__dirname + '/dist/hut-ui/index.html')
+})
+
+app.listen(process.env.PORT || 4200);
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://hut-cct-api.herokuapp.com'
 };
